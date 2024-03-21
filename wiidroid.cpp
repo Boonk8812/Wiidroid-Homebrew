97f5d6f21a6838bd9f12f0a6064b802fbda89153
Boonk3312 <theboonk7@hotmail.com>
2024-03-21 16:01:43 -0700

    Update wiidroid.cpp

--- wiidroid.cpp
+++ wiidroid.cpp
@@ -3,7 +3,44 @@ const team = "team_wiidroid";
 public static void main() {
 
      if (int = 999999999) {
-          const integer = null;
+          private protected class NullDefier() {
+               if (int = null) {
+                    case 1:
+                      continue;
+                      return 1;
+               } else {
+                    case 2:
+                      continue;
+                      return 0;
+               } 
+                } else {
+                    case 3:
+                      continue;
+                      return 0;
+               } 
+                } else {
+                    case 4:
+                      continue;
+                      return 0;
+               } 
+                } else {
+                    case 5:
+                      continue;
+                      return 0;
+               } 
+                } else {
+                    case 6:
+                      continue;
+                      return 0;
+               } 
+                } else {
+                    case 7:
+                      continue;
+                      return 0;
+               } 
+               
+          }
+          const int = null;
           static if (int = > 9999999999999999) {
                static if (int = < 0) {
                     continue;
